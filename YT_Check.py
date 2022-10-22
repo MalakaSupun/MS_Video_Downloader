92f61badead6007e78d275dc3a93d12dc63019de
MS Repositories <71941117+MalakaSupun@users.noreply.github.com>
2022-10-23 01:07:12 +0530

    Update YT_Check.py

--- YT_Check.py
+++ YT_Check.py
@@ -5,6 +5,7 @@ import yt_dlp
 import json
 import requests
 import time
+import os
 import concurrent.futures
 
 '''Formats = ['bestvideo*+bestaudio/best', 'bestaudio/best',
@@ -12,9 +13,11 @@ import concurrent.futures
            'bestvideo[height<=480]+bestaudio/best[height<=480]', 'bestvideo[height<=360]+bestaudio/best[height<=360]',
            'bestvideo[height<=244]+bestaudio/best[height<=244]', 'bestvideo[height<=144]+bestaudio/best[height<=144]']'''
 
-Alw_Sub_Languages = ['en', 'si', 'ta']
 lang = []
 Auto_langs = []
+
+Alw_Sub_Languages = ['en', 'si', 'ta']
+Auto_Sub_Langs = []
 Not_In_Direct_Sub = []
 Not_In_Auto_Sub = []
 
@@ -26,6 +29,8 @@ class Thread_ChecksUV(QThread):
         QThread.__init__(self, parent)
         self.MainCode = parent
 
+        self.Filtered_Auto_Subs = []
+        self.Available_Subs = []
         self.Video_checked = 0
         self.V240p_Size = " "
         self.V360p_Size = " "
@@ -43,19 +48,23 @@ class Thread_ChecksUV(QThread):
         self.YT_title = " "
         self.BestV_Size = " "
         self.BestV_Format = " "
-        self.V144p_Size = " "
+        self.V144p_Size = "Not Available"
         self.Audio_Size = " "
         self.Audio_Format = " "
         self.The_link = " "
         self.F_Requested = " "
         # self.Formats = " "
+        self.Commands_for_subs = []
+        self.Filtered_Direct_Subs = []
 
     def run(self):
+        self.ClearLists()
         self.MainCode.progressBar_YT_V.setValue(0)
         self.MainCode.label_Pcomplete_UTV.setText("................... Video Checking ...................")
         print("................... Video Checking ...................")
 
         S_Time = time.perf_counter()
+        self.Set_Default_Labels()
 
         self.F_Requested = self.MainCode.comboBox_Quality_YV.currentIndex()
         link = self.MainCode.lineEdit_YTV_link.text()
@@ -80,6 +89,8 @@ class Thread_ChecksUV(QThread):
             self.Set_Lables()
             self.set_Selected_Video_Size()
             print(f'Execution Time : {round(F_Time - S_Time, 3)} s')
+            self.Video_checked = 1
+            self.ClearLists()
 
         else:
             print("Add Link to Download / Check")
@@ -88,14 +99,11 @@ class Thread_ChecksUV(QThread):
 
     def Sub_langs(self, item):
         if item == 'en':
-            ls = "English"
-            return ls
+            return 'en'
         elif item == 'si':
-            ld = "Sinhala"
-            return ld
+            return 'en'
         elif item == 'ta':
-            lz = "Tamil"
-            return lz
+            return 'en'
 
     def get_Video_info(self, VideoFormat):
         # 'quiet': True,
@@ -144,18 +152,23 @@ class Thread_ChecksUV(QThread):
             if VideoFormat == 'bestvideo[height<=144]+bestaudio/best[height<=144]':
                 self.V144p_Size = round(int(B["filesize_approx"]) / (1024 * 1024), 2)
 
-    def Set_Lables(self):
+    def ClearLists(self):
+        lang.clear()
+        Auto_langs.clear()
+        Auto_Sub_Langs.clear()
+        self.Filtered_Direct_Subs.clear()
+        self.Filtered_Auto_Subs.clear()
+        self.Available_Subs.clear()
+        self.Commands_for_subs.clear()
 
+    def Set_Lables(self):
+        self.ClearLists()
         Video_title = self.YT_title
         Youtube_Channel = self.Yt_Channle
         View_Count = self.YTV_ViewCount
         Uploaded_Date = self.Uploded_Date
         Thumbnail_URL = self.Thumbnail_URL
         Video_Duration = self.Video_Duration
-
-        # Sub titles of the videos.......................................
-        # Direct_Sub_Availability = self.Direct_Sub_Avalability
-        # Auto_Sub_Availability = self.Auto_Sub_Avalability
 
         Video_Best_Format = self.BestV_Format
         V_Size_Best = self.BestV_Size
@@ -166,12 +179,53 @@ class Thread_ChecksUV(QThread):
         V_Size_240p = self.V240p_Size
         V_Size_144p = self.V144p_Size
 
-        self.MainCode.label_Title_YTV.setText(f'Video Title : {Video_title}')
-        self.MainCode.label_Views_YTV.setText(f'Video Views : {View_Count:,}')
-        self.MainCode.label_Channel_YTV.setText(f'Channel : {Youtube_Channel}')
-        self.MainCode.label_Duration_YTV.setText(f'Video Duration(H:M:S) : {Video_Duration} ')
-        self.MainCode.label_Date_YTV.setText(f'Uploaded Date : {Uploaded_Date}')
-        self.MainCode.label_Channel_YTV_BTV.setText(f'Best format available:  {Video_Best_Format} ')
+        try:
+            thumbstT = time.perf_counter()
+            # Thumbnail_Link = B["thumbnail"]
+            response = requests.get(Thumbnail_URL)
+            # name = "YTV_Thumbnail.jpg"
+            with open("YTV_Thumbnail.jpg", "wb") as thumbnail:
+                thumbnail.write(response.content)
+
+            self.MainCode.Youtube_Vthumbnail.setPixmap(QPixmap('YTV_Thumbnail.jpg'))
+            thumbEdT = time.perf_counter()
+            print(round(thumbEdT - thumbstT, 3))  # 0.443s
+
+        except:
+            # if self.Video_checked == 1:
+            try:
+                try:
+                    os.remove(
+                        r'E:\other\Python\Projects\Youtube_Downloader_Yt_dlp\Thumbnail\Youtube_Video\YoutubeVideoThumb.jpg')
+                    os.remove(
+                        r'E:\other\Python\Projects\Youtube_Downloader_Yt_dlp\Thumbnail\Youtube_Video\YoutubeVideoThumb.webp')
+                except:
+                    os.remove(
+                        r'E:\other\Python\Projects\Youtube_Downloader_Yt_dlp\Thumbnail\Youtube_Video\YoutubeVideoThumb.webp')
+            except:
+                print("........ No previous thumbnail images ........")
+
+            # 17.122s
+            Video_folder = r'Thumbnail\Youtube_Video'
+            ydl_opts = {'writethumbnail': True,
+                        'noplaylist': True,
+                        'outtmpl': {'default': f'{Video_folder}/YoutubeVideoThumb.%(ext)s'},
+                        'skip_download': True, 'ignoreerrors': True, 'no_warnings': True
+                        }
+
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(self.The_link)
+            try:
+                self.MainCode.Youtube_Vthumbnail.setPixmap(QPixmap(r'Thumbnail\Youtube_Video\YoutubeVideoThumb'))
+            except:
+                self.MainCode.Youtube_Vthumbnail.setPixmap(QPixmap(r'Icons\Tumbs\minis.png'))
+
+        self.MainCode.label_Title_YTV.setText(f'{Video_title}')
+        self.MainCode.label_Views_YTV.setText(f'{View_Count:,}')
+        self.MainCode.label_Channel_YTV.setText(f'{Youtube_Channel}')
+        self.MainCode.label_Duration_YTV.setText(f'{Video_Duration}')
+        self.MainCode.label_Date_YTV.setText(f'{Uploaded_Date}')
+        self.MainCode.label_Channel_YTV_BTV.setText(f'{Video_Best_Format}')
 
         try:
             Direct_Sub_Availability = self.Direct_Sub_Avalability
@@ -181,74 +235,84 @@ class Thread_ChecksUV(QThread):
                 elif a == 'live_chat':
                     raise KeyError
 
-            # self.MainCode.label_Channel_YTV_BTV_Subs.setText(f'Subtitles : Not-Available')
             self.MainCode.radioButton_UTV_subs.setDisabled(False)
-            print('\nDirect sub available..... ')
             print(f"Direct sub langs: {lang}")
 
-            Available_Direct_Subs = [a for a in lang if self.Sub_langs(a)]
-            print(f"Available direct sub langs: {Available_Direct_Subs}")
-            if Available_Direct_Subs.__len__() == 0:
+            self.Filtered_Direct_Subs = [a for a in lang if self.Sub_langs(a)]
+
+            print(f"Available direct sub langs: {self.Filtered_Direct_Subs}")
+            if self.Filtered_Direct_Subs.__len__() == 0:
                 print("No Direct Subtitles Available \n................... Key Error! ...................")
                 raise KeyError
-            if Available_Direct_Subs.__len__() != 0:
-                en_c = Available_Direct_Subs.count('en')
-                si_c = Available_Direct_Subs.count('si')
-                ta_c = Available_Direct_Subs.count('ta')
-                if en_c == 0:
-                    Not_In_Direct_Sub.append("English")
-                if si_c == 0:
-                    Not_In_Direct_Sub.append("Sinhala")
-                if ta_c == 0:
-                    Not_In_Direct_Sub.append("Tamil")
-                '''if en_c or si_c or ta_c == 0:
-                    raise KeyError'''
-                print(f"Sub Titles not in the direct subs : {Not_In_Direct_Sub}")
+            if self.Filtered_Direct_Subs.__len__() != 0:
 
-            Auto_Sub_Availability = self.Auto_Sub_Avalability
-            for y in Auto_Sub_Availability:
-                Auto_langs.append(y)
-            print(f"Auto Sub Titles :{Auto_langs}")
-            Available_Auto_Subs = [b for b in Auto_langs if self.Sub_langs(b)]
-            print(f"Available direct sub langs: {Available_Auto_Subs}")
-            self.MainCode.label_Channel_YTV_BTV_Subs.setText(f'Subtitles : Available')
+                for i in self.Filtered_Direct_Subs:
+                    if i == 'en':
+                        self.Available_Subs.append("English")
+                    elif i == 'si':
+                        self.Available_Subs.append("Sinhala")
+                    elif i == 'ta':
+                        self.Available_Subs.append("Tamil")
+            self.Commands_for_subs = self.Filtered_Direct_Subs.copy()
 
+            if len(self.Filtered_Direct_Subs) != 3:
+                Auto_Sub_Availability = self.Auto_Sub_Avalability
+                for y in Auto_Sub_Availability:
+                    Auto_Sub_Langs.append(y)
+
+                self.Filtered_Auto_Subs = [a for a in Auto_Sub_Langs if self.Sub_langs(a)]
+                print(f"Filtered Auto Subs : {self.Filtered_Auto_Subs}")
+                self.Commands_for_subs.extend(self.Filtered_Auto_Subs.copy())
+                for i in self.Filtered_Auto_Subs:
+                    if i == 'en':
+                        self.Available_Subs.append("English-Auto-Gen")
+                    elif i == 'si':
+                        self.Available_Subs.append("Sinhala-Auto-Gen")
+                    elif i == 'ta':
+                        self.Available_Subs.append("Tamil-Auto-Gen")
+
+            self.MainCode.label_Channel_YTV_BTV_Subs.setText(f'Available')
+            print(f'Available Subs : {self.Available_Subs}')
+            print(f"Commands For Available Subs : {self.Commands_for_subs}")
 
         except KeyError:
             try:
                 Auto_Sub_Availability = self.Auto_Sub_Avalability
                 for y in Auto_Sub_Availability:
                     Auto_langs.append(y)
+
                 print(f"All Available Auto Sub Titles :{Auto_langs}")
 
-                Available_Auto_Subs = [b for b in Auto_langs if self.Sub_langs(b)]
-                print(f"Available Auto sub langs: {Available_Auto_Subs}")
+                self.Filtered_Auto_Subs = [b for b in Auto_langs if self.Sub_langs(b)]
+                print(f"Available Auto sub langs: {self.Filtered_Auto_Subs}")
 
-                if Available_Auto_Subs.__len__() == 0:
-                    self.MainCode.label_Channel_YTV_BTV_Subs.setText(f'Subtitles : Not Available')
+                self.Commands_for_subs.extend(self.Filtered_Auto_Subs.copy())
+
+                if self.Filtered_Auto_Subs.__len__() == 0:
+                    self.MainCode.label_Channel_YTV_BTV_Subs.setText(f' Not Available')
                     self.MainCode.radioButton_UTV_subs.setDisabled(True)
                 else:
-                    self.MainCode.label_Channel_YTV_BTV_Subs.setText(f'Subtitles : Available')
+                    for i in self.Filtered_Auto_Subs:
+                        if i == 'en':
+                            self.Available_Subs.append("English-Auto-Gen")
+                        elif i == 'si':
+                            self.Available_Subs.append("Sinhala-Auto-Gen")
+                        elif i == 'ta':
+                            self.Available_Subs.append("Tamil-Auto-Gen")
+
+                    self.MainCode.label_Channel_YTV_BTV_Subs.setText(f' Available')
                     self.MainCode.radioButton_UTV_subs.setDisabled(False)
-                    en_ca = Available_Auto_Subs.count('en')
-                    si_ca = Available_Auto_Subs.count('si')
-                    ta_ca = Available_Auto_Subs.count('ta')
-                    if en_ca == 0:
-                        Not_In_Auto_Sub.append("English")
-                    if si_ca == 0:
-                        Not_In_Auto_Sub.append("Sinhala")
-                    if ta_ca == 0:
-                        Not_In_Auto_Sub.append("Tamil")
-                    print(f"Sub Titles not in the auto subs : {Not_In_Auto_Sub}")
+                    print(f'Available Subs : {self.Available_Subs}')
+                    print(f"Commands For Available Subs : {self.Commands_for_subs}")
             except:
                 print("........No subtitles available for this video...........")
                 self.MainCode.radioButton_UTV_subs.setDisabled(True)
-                self.MainCode.label_Channel_YTV_BTV_Subs.setText(f'Subtitles : Not Available')
+                self.MainCode.label_Channel_YTV_BTV_Subs.setText(f' Not Available')
 
         except:
             print("........No subtitles available for this video...........")
             self.MainCode.radioButton_UTV_subs.setDisabled(True)
-            self.MainCode.label_Channel_YTV_BTV_Subs.setText(f'Subtitles : Not Available')
+            self.MainCode.label_Channel_YTV_BTV_Subs.setText(f' Not Available')
 
         self.MainCode.comboBox_Quality_YV.clear()
         Q1s = [f' Best Audio & Video  < {V_Size_Best} MB >',
@@ -260,18 +324,12 @@ class Thread_ChecksUV(QThread):
                f' 144p                         < {V_Size_144p} MB >']
 
         self.MainCode.comboBox_Quality_YV.addItems(Q1s)
+
+        self.MainCode.comboBox_Quality_Sub_YV.clear()
+        self.MainCode.comboBox_Quality_Sub_YV.addItems(self.Available_Subs)
+
         self.MainCode.comboBox_Quality_YV.setCurrentIndex(self.F_Requested)
 
-        try:
-            # Thumbnail_Link = B["thumbnail"]
-            response = requests.get(Thumbnail_URL)
-            # name = "YTV_Thumbnail.jpg"
-            with open("YTV_Thumbnail.jpg", "wb") as thumbnail:
-                thumbnail.write(response.content)
-
-            self.MainCode.Youtube_Vthumbnail.setPixmap(QPixmap('YTV_Thumbnail.jpg'))
-        except:
-            self.MainCode.Youtube_Vthumbnail.setPixmap(QPixmap('Icons/Thumbnail.png'))
         # print(B["requested_subtitles"])
 
         self.MainCode.label_Pcomplete_UTV.setText("................... Video Checked ...................")
@@ -304,3 +362,16 @@ class Thread_ChecksUV(QThread):
             if self.F_Requested == 6:
                 self.MainCode.label_Views_Selected_YTV_Size.setText(
                     f'Size of video selected : {self.V144p_Size}  MB')
+
+    def Set_Default_Labels(self):
+        self.MainCode.label_Title_YTV.setText('')
+        self.MainCode.label_Views_YTV.setText('')
+        self.MainCode.label_Channel_YTV.setText('')
+        self.MainCode.label_Duration_YTV.setText('')
+        self.MainCode.label_Date_YTV.setText('')
+        self.MainCode.label_Channel_YTV_BTV_Subs.setText('')
+        self.MainCode.label_Channel_YTV_BTV.setText('')
+        self.MainCode.label_DBytes_YTV.setText("0 KB")
+        self.MainCode.label_DSpeed_YTV_2.setText('0 MiB/s')
+        self.MainCode.label_RTime_YTV_2.setText('00:00:00')
+
