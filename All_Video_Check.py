9af7422b459b4eca078854a348840db80be4985e
MS Repositories <71941117+MalakaSupun@users.noreply.github.com>
2022-11-12 23:38:55 +0530

    Update All_Video_Check.py

--- All_Video_Check.py
+++ All_Video_Check.py
@@ -26,3 +26,12 @@ Formats = ['bestvideo*+bestaudio/best', 'bestaudio/best',
            'bestvideo[height<=480]+bestaudio/best[height<=480]', 'bestvideo[height<=360]+bestaudio/best[height<=360]',
            'bestvideo[height<=240]+bestaudio/best[height<=240]', 'bestvideo[height<=144]+bestaudio/best[height<=144]']
 
+
+lang = []
+Auto_langs = []
+# Subtitles that need to download......
+Alw_Sub_Languages = ['en', 'si', 'ta']
+Auto_Sub_Langs = []
+Not_In_Direct_Sub = []
+Not_In_Auto_Sub = []
+
