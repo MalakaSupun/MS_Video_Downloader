3d1864ff51ae454a80c8cfb82a39298199c1c3e1
MS Repositories <71941117+MalakaSupun@users.noreply.github.com>
2023-03-12 02:29:33 +0530

    Update FB_Download.py

--- FB_Download.py
+++ FB_Download.py
@@ -21,10 +21,13 @@ from plyer import
 
 
 # All the formats ..........................
-Formats = ['bestvideo*+bestaudio/best', 'bestaudio/best',
-           'bestvideo[height<=1080]+bestaudio/best[height<=1080]', 'bestvideo[height<=720]+bestaudio/best[height<=720]',
-           'bestvideo[height<=480]+bestaudio/best[height<=480]', 'bestvideo[height<=360]+bestaudio/best[height<=360]',
-           'bestvideo[height<=240]+bestaudio/best[height<=240]', 'bestvideo[height<=144]+bestaudio/best[height<=144]']
+Formats = ['bestvideo*+bestaudio/best', 'bestaudio/best',                                         # Best video and audios
+           'bestvideo[height<=1080]+bestaudio/best[height<=1080]',                                # 1080p                                               
+           'bestvideo[height<=720]+bestaudio/best[height<=720]',                                  # 720p
+           'bestvideo[height<=480]+bestaudio/best[height<=480]',                                  # 480p
+           'bestvideo[height<=360]+bestaudio/best[height<=360]',                                  # 360p
+           'bestvideo[height<=240]+bestaudio/best[height<=240]',                                  # 240p
+           'bestvideo[height<=144]+bestaudio/best[height<=144]']                                  # 144p
 
 
 
